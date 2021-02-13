75429e610dd4ead691f4a53f96c31c16c8affa13
Duane Cilliers <hello@duane.co.za>
2021-02-13 13:05:04 +0200

    feat: create placeholder objects in splash scene

--- src/components/canvas/Splash.js
+++ src/components/canvas/Splash.js
@@ -1,17 +1,36 @@
-import { Environment, Sphere } from '@react-three/drei'
-import React from 'react'
+import { Environment, OrbitControls, Sphere } from '@react-three/drei'
+import { range } from 'ramda'
+import React, { useRef } from 'react'
 import { Suspense } from 'react-is'
+import { useFrame } from 'react-three-fiber'
 
-const PlaceHolderMesh = ({ position = [0, 1, 0] }) => {
+const PlaceHolderMesh = ({
+  args = [0.1, 32, 32],
+  position = [-0.5, 0, 0.5],
+}) => {
   return (
-    <mesh position={position}>
-      <meshNormalMaterial attach='material' />
-    </mesh>
+    <Sphere args={args} position={position}>
+      <meshBasicMaterial attach='material' />
+    </Sphere>
   )
 }
 
 const Splash = ({ color }) => {
-  console.log('color', color)
+  const objectsRef = useRef()
+
+  // useFrame((time) => {
+  //   objectsRef.rotation.x = objectsRef.rotation.x * time
+  // })
+
+  const randomPoint = () => (Math.random() - 0.5) * 5
+
+  const Objects = () =>
+    range(0, 20)
+      .map(() => ({
+        position: [randomPoint(), randomPoint(), randomPoint()],
+      }))
+      .map((obj, i) => <PlaceHolderMesh key={i} position={obj.position} />)
+
   return (
     <Suspense fallback={null}>
       <ambientLight intensity={0.5} />
@@ -19,8 +38,11 @@ const Splash = ({ color }) => {
       <Sphere args={[1, 32, 32]}>
         <meshToonMaterial color={color} />
       </Sphere>
-      <PlaceHolderMesh />
-      <Environment preset={'studio'} />
+      <Objects />
+      <group ref={objectsRef}>
+        <Environment preset={'studio'} />
+      </group>
+      <OrbitControls />
     </Suspense>
   )
 }
